a9892d769610fdd2367d8400de35b2fed5000679
Mikołaj <mikolaj.pura18@gmail.com>
2025-01-06 13:26:38 +0100

    Initial commit

---
+++ src/main/java/com/pura/Pura/PuraApplication.java
@@ -0,0 +1,13 @@
+package com.pura.Pura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pura/Pura/PuraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pura.Pura;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
